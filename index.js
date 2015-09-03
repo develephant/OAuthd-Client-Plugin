88e7056973cb8ed7c27bc22ed3c492f1f0b01337
Chris Byerley <cbyerley@develephant.com>
2015-09-02 23:35:31 -0500

    0.0.19

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
+var ecstatic, fs, restify;
+restify = require('restify');
+ecstatic = require('ecstatic');
+fs = require('fs');
+
 module.exports = function(env) {
-	var fs = require('fs');
-	var restify = require('restify');
 	var api =
 	{
 		init: function() {
-			env.server.get(/^(\/.*)/, function( req, res ) {
-				var path = req.params[0];
-				console.log( path );
-				//Reroute base to client
-				if ( path === '/' ) {
-					res.setHeader("Location", "/client");
-					res.send( 302 );
-				}
-				
-				//Check the incoming path and see if a file is available
-				//If so serve it with static, if not found try index.html
-				fs.stat( __dirname + '/public' + path, function( err, stat ) {
-					if ( stat != null && stat.isFile() ) {
-						//Go static
-						restify.serveStatic({
-							directory: __dirname + '/public',
-							"default": __dirname + '/public/index.html'
-						});
-					} else {
-						//Serve up index.html
-						fs.readFile( __dirname + '/public/index.html', {encoding:'UTF-8'}, function( err, data ) {
-							res.setHeader("Content-Type", "text/html");
-							res.send( 200 , data );
-						});
-					}
-				});
-			});
+			
+			
+			env.server.get(/^(\/.*)/, function(req, res, next) {
+      if (req.params[0] === '/') {
+        res.setHeader('Location', '/home');
+        res.send(302);
+      }
+      
+			fs.stat(__dirname + '/public' + req.params[0], function(err, stat) {
+        if (stat != null ? stat.isFile() : void 0) {
+          next();
+        } else {
+          fs.readFile(__dirname + '/public/index.html', {
+            encoding: 'UTF-8'
+          }, function(err, data) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(200, data);
+          });
+        }
+      });
+    }, restify.serveStatic({
+      directory: __dirname + '/public',
+      "default": __dirname + '/public/index.html'
+    }));
+
 		}
 	};
 	
---
+++ index.old.js
@@ -0,0 +1,41 @@
+module.exports = function(env) {
+	var fs = require('fs');
+	var restify = require('restify');
+	var api =
+	{
+		init: function() {
+			env.server.get(/^(\/.*)/, function( req, res ) {
+					
+			}
+			// env.server.get(/^(\/.*)/, function( req, res ) {
+			// 	var path = req.params[0];
+			// 	console.log( path );
+			// 	//Reroute base to client
+			// 	if ( path === '/' ) {
+			// 		res.setHeader("Location", "/client");
+			// 		res.send( 302 );
+			// 	}
+			//
+			// 	//Check the incoming path and see if a file is available
+			// 	//If so serve it with static, if not found try index.html
+			// 	fs.stat( __dirname + '/public' + path, function( err, stat ) {
+			// 		if ( stat != null && stat.isFile() ) {
+			// 			//Go static
+			// 			restify.serveStatic({
+			// 				directory: __dirname + '/public',
+			// 				"default": __dirname + '/public/index.html'
+			// 			});
+			// 		} else {
+			// 			//Serve up index.html
+			// 			fs.readFile( __dirname + '/public/index.html', {encoding:'UTF-8'}, function( err, data ) {
+			// 				res.setHeader("Content-Type", "text/html");
+			// 				res.send( 200 , data );
+			// 			});
+			// 		}
+			// 	});
+			// });
+		}
+	};
+	
+	return api;
+}

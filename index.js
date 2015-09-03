85174adf16545d6c7f4ef49cc7b12ef86e747b3a
Chris Byerley <cbyerley@develephant.com>
2015-09-02 21:53:53 -0500

    0.0.5

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ module.exports = function(env) {
 		init: function() {
 			env.server.get('/client', function( req, res ) {
 				//load index file
-				fs.readFile(__dirname + 'public/index.html', {encoding:'UTF-8'}, function( err, data ) {
+				fs.readFile( __dirname + 'public/index.html', {encoding:'UTF-8'}, function( err, data ) {
 					if ( err ) {
-						res.send(404, 'File not found!');
+						res.send(404, err);
 					} else {
 						res.send(200, data);
 					}

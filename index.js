f61cb19db40cda1a72c7d4c187037717305eaea2
Chris Byerley <cbyerley@develephant.com>
2015-09-02 21:58:34 -0500

    0.0.6

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var fs = require('fs');
 module.exports = function(env) {
-	api =
+	var fs = require('fs');
+	var api =
 	{
 		init: function() {
 			env.server.get('/client', function( req, res ) {
 				//load index file
 				fs.readFile( __dirname + 'public/index.html', {encoding:'UTF-8'}, function( err, data ) {
 					if ( err ) {
-						res.send(404, err);
+						res.send( err );
 					} else {
-						res.send(200, data);
+						res.send( data );
 					}
 				});
 			});
